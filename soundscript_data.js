3511f8a378b7b8dbcce7217d49671efe30375761
Marcia Suzuki <marcia.suzuki@uofn.edu>
2026-01-18 10:46:34 -0600

    Add files via upload

---
+++ soundscript_data.js
@@ -0,0 +1,1002 @@
+const MOTIF_DATA = [
+  {
+    "id": 29,
+    "symbol": "M29",
+    "count": 266472,
+    "samples": [
+      "motif_29_sample_0.wav",
+      "motif_29_sample_1.wav",
+      "motif_29_sample_2.wav"
+    ]
+  },
+  {
+    "id": 82,
+    "symbol": "M82",
+    "count": 217421,
+    "samples": [
+      "motif_82_sample_0.wav",
+      "motif_82_sample_1.wav",
+      "motif_82_sample_2.wav"
+    ]
+  },
+  {
+    "id": 26,
+    "symbol": "M26",
+    "count": 179759,
+    "samples": [
+      "motif_26_sample_0.wav",
+      "motif_26_sample_1.wav",
+      "motif_26_sample_2.wav"
+    ]
+  },
+  {
+    "id": 81,
+    "symbol": "M81",
+    "count": 161271,
+    "samples": [
+      "motif_81_sample_0.wav",
+      "motif_81_sample_1.wav",
+      "motif_81_sample_2.wav"
+    ]
+  },
+  {
+    "id": 85,
+    "symbol": "M85",
+    "count": 158889,
+    "samples": [
+      "motif_85_sample_0.wav",
+      "motif_85_sample_1.wav",
+      "motif_85_sample_2.wav"
+    ]
+  },
+  {
+    "id": 55,
+    "symbol": "M55",
+    "count": 153712,
+    "samples": [
+      "motif_55_sample_0.wav",
+      "motif_55_sample_1.wav",
+      "motif_55_sample_2.wav"
+    ]
+  },
+  {
+    "id": 18,
+    "symbol": "M18",
+    "count": 148241,
+    "samples": [
+      "motif_18_sample_0.wav",
+      "motif_18_sample_1.wav",
+      "motif_18_sample_2.wav"
+    ]
+  },
+  {
+    "id": 62,
+    "symbol": "M62",
+    "count": 138527,
+    "samples": [
+      "motif_62_sample_0.wav",
+      "motif_62_sample_1.wav",
+      "motif_62_sample_2.wav"
+    ]
+  },
+  {
+    "id": 44,
+    "symbol": "M44",
+    "count": 131139,
+    "samples": [
+      "motif_44_sample_0.wav",
+      "motif_44_sample_1.wav",
+      "motif_44_sample_2.wav"
+    ]
+  },
+  {
+    "id": 23,
+    "symbol": "M23",
+    "count": 130346,
+    "samples": [
+      "motif_23_sample_0.wav",
+      "motif_23_sample_1.wav",
+      "motif_23_sample_2.wav"
+    ]
+  },
+  {
+    "id": 19,
+    "symbol": "M19",
+    "count": 125666,
+    "samples": [
+      "motif_19_sample_0.wav",
+      "motif_19_sample_1.wav",
+      "motif_19_sample_2.wav"
+    ]
+  },
+  {
+    "id": 16,
+    "symbol": "M16",
+    "count": 122186,
+    "samples": [
+      "motif_16_sample_0.wav",
+      "motif_16_sample_1.wav",
+      "motif_16_sample_2.wav"
+    ]
+  },
+  {
+    "id": 1,
+    "symbol": "M01",
+    "count": 119696,
+    "samples": [
+      "motif_01_sample_0.wav",
+      "motif_01_sample_1.wav",
+      "motif_01_sample_2.wav"
+    ]
+  },
+  {
+    "id": 78,
+    "symbol": "M78",
+    "count": 118846,
+    "samples": [
+      "motif_78_sample_0.wav",
+      "motif_78_sample_1.wav",
+      "motif_78_sample_2.wav"
+    ]
+  },
+  {
+    "id": 39,
+    "symbol": "M39",
+    "count": 116044,
+    "samples": [
+      "motif_39_sample_0.wav",
+      "motif_39_sample_1.wav",
+      "motif_39_sample_2.wav"
+    ]
+  },
+  {
+    "id": 7,
+    "symbol": "M07",
+    "count": 113989,
+    "samples": [
+      "motif_07_sample_0.wav",
+      "motif_07_sample_1.wav",
+      "motif_07_sample_2.wav"
+    ]
+  },
+  {
+    "id": 31,
+    "symbol": "M31",
+    "count": 110900,
+    "samples": [
+      "motif_31_sample_0.wav",
+      "motif_31_sample_1.wav",
+      "motif_31_sample_2.wav"
+    ]
+  },
+  {
+    "id": 10,
+    "symbol": "M10",
+    "count": 110243,
+    "samples": [
+      "motif_10_sample_0.wav",
+      "motif_10_sample_1.wav",
+      "motif_10_sample_2.wav"
+    ]
+  },
+  {
+    "id": 84,
+    "symbol": "M84",
+    "count": 108609,
+    "samples": [
+      "motif_84_sample_0.wav",
+      "motif_84_sample_1.wav",
+      "motif_84_sample_2.wav"
+    ]
+  },
+  {
+    "id": 20,
+    "symbol": "M20",
+    "count": 107791,
+    "samples": [
+      "motif_20_sample_0.wav",
+      "motif_20_sample_1.wav",
+      "motif_20_sample_2.wav"
+    ]
+  },
+  {
+    "id": 22,
+    "symbol": "M22",
+    "count": 107147,
+    "samples": [
+      "motif_22_sample_0.wav",
+      "motif_22_sample_1.wav",
+      "motif_22_sample_2.wav"
+    ]
+  },
+  {
+    "id": 5,
+    "symbol": "M05",
+    "count": 106500,
+    "samples": [
+      "motif_05_sample_0.wav",
+      "motif_05_sample_1.wav",
+      "motif_05_sample_2.wav"
+    ]
+  },
+  {
+    "id": 15,
+    "symbol": "M15",
+    "count": 100602,
+    "samples": [
+      "motif_15_sample_0.wav",
+      "motif_15_sample_1.wav",
+      "motif_15_sample_2.wav"
+    ]
+  },
+  {
+    "id": 67,
+    "symbol": "M67",
+    "count": 98954,
+    "samples": [
+      "motif_67_sample_0.wav",
+      "motif_67_sample_1.wav",
+      "motif_67_sample_2.wav"
+    ]
+  },
+  {
+    "id": 52,
+    "symbol": "M52",
+    "count": 98135,
+    "samples": [
+      "motif_52_sample_0.wav",
+      "motif_52_sample_1.wav",
+      "motif_52_sample_2.wav"
+    ]
+  },
+  {
+    "id": 76,
+    "symbol": "M76",
+    "count": 97286,
+    "samples": [
+      "motif_76_sample_0.wav",
+      "motif_76_sample_1.wav",
+      "motif_76_sample_2.wav"
+    ]
+  },
+  {
+    "id": 4,
+    "symbol": "M04",
+    "count": 94742,
+    "samples": [
+      "motif_04_sample_0.wav",
+      "motif_04_sample_1.wav",
+      "motif_04_sample_2.wav"
+    ]
+  },
+  {
+    "id": 99,
+    "symbol": "M99",
+    "count": 93872,
+    "samples": [
+      "motif_99_sample_0.wav",
+      "motif_99_sample_1.wav",
+      "motif_99_sample_2.wav"
+    ]
+  },
+  {
+    "id": 27,
+    "symbol": "M27",
+    "count": 92758,
+    "samples": [
+      "motif_27_sample_0.wav",
+      "motif_27_sample_1.wav",
+      "motif_27_sample_2.wav"
+    ]
+  },
+  {
+    "id": 56,
+    "symbol": "M56",
+    "count": 90033,
+    "samples": [
+      "motif_56_sample_0.wav",
+      "motif_56_sample_1.wav",
+      "motif_56_sample_2.wav"
+    ]
+  },
+  {
+    "id": 46,
+    "symbol": "M46",
+    "count": 88857,
+    "samples": [
+      "motif_46_sample_0.wav",
+      "motif_46_sample_1.wav",
+      "motif_46_sample_2.wav"
+    ]
+  },
+  {
+    "id": 92,
+    "symbol": "M92",
+    "count": 87821,
+    "samples": [
+      "motif_92_sample_0.wav",
+      "motif_92_sample_1.wav",
+      "motif_92_sample_2.wav"
+    ]
+  },
+  {
+    "id": 66,
+    "symbol": "M66",
+    "count": 86570,
+    "samples": [
+      "motif_66_sample_0.wav",
+      "motif_66_sample_1.wav",
+      "motif_66_sample_2.wav"
+    ]
+  },
+  {
+    "id": 57,
+    "symbol": "M57",
+    "count": 86036,
+    "samples": [
+      "motif_57_sample_0.wav",
+      "motif_57_sample_1.wav",
+      "motif_57_sample_2.wav"
+    ]
+  },
+  {
+    "id": 8,
+    "symbol": "M08",
+    "count": 85965,
+    "samples": [
+      "motif_08_sample_0.wav",
+      "motif_08_sample_1.wav",
+      "motif_08_sample_2.wav"
+    ]
+  },
+  {
+    "id": 2,
+    "symbol": "M02",
+    "count": 85535,
+    "samples": [
+      "motif_02_sample_0.wav",
+      "motif_02_sample_1.wav",
+      "motif_02_sample_2.wav"
+    ]
+  },
+  {
+    "id": 32,
+    "symbol": "M32",
+    "count": 84635,
+    "samples": [
+      "motif_32_sample_0.wav",
+      "motif_32_sample_1.wav",
+      "motif_32_sample_2.wav"
+    ]
+  },
+  {
+    "id": 93,
+    "symbol": "M93",
+    "count": 84411,
+    "samples": [
+      "motif_93_sample_0.wav",
+      "motif_93_sample_1.wav",
+      "motif_93_sample_2.wav"
+    ]
+  },
+  {
+    "id": 53,
+    "symbol": "M53",
+    "count": 84202,
+    "samples": [
+      "motif_53_sample_0.wav",
+      "motif_53_sample_1.wav",
+      "motif_53_sample_2.wav"
+    ]
+  },
+  {
+    "id": 72,
+    "symbol": "M72",
+    "count": 83793,
+    "samples": [
+      "motif_72_sample_0.wav",
+      "motif_72_sample_1.wav",
+      "motif_72_sample_2.wav"
+    ]
+  },
+  {
+    "id": 30,
+    "symbol": "M30",
+    "count": 83428,
+    "samples": [
+      "motif_30_sample_0.wav",
+      "motif_30_sample_1.wav",
+      "motif_30_sample_2.wav"
+    ]
+  },
+  {
+    "id": 34,
+    "symbol": "M34",
+    "count": 82479,
+    "samples": [
+      "motif_34_sample_0.wav",
+      "motif_34_sample_1.wav",
+      "motif_34_sample_2.wav"
+    ]
+  },
+  {
+    "id": 14,
+    "symbol": "M14",
+    "count": 82009,
+    "samples": [
+      "motif_14_sample_0.wav",
+      "motif_14_sample_1.wav",
+      "motif_14_sample_2.wav"
+    ]
+  },
+  {
+    "id": 42,
+    "symbol": "M42",
+    "count": 81058,
+    "samples": [
+      "motif_42_sample_0.wav",
+      "motif_42_sample_1.wav",
+      "motif_42_sample_2.wav"
+    ]
+  },
+  {
+    "id": 90,
+    "symbol": "M90",
+    "count": 80865,
+    "samples": [
+      "motif_90_sample_0.wav",
+      "motif_90_sample_1.wav",
+      "motif_90_sample_2.wav"
+    ]
+  },
+  {
+    "id": 65,
+    "symbol": "M65",
+    "count": 80026,
+    "samples": [
+      "motif_65_sample_0.wav",
+      "motif_65_sample_1.wav",
+      "motif_65_sample_2.wav"
+    ]
+  },
+  {
+    "id": 58,
+    "symbol": "M58",
+    "count": 79968,
+    "samples": [
+      "motif_58_sample_0.wav",
+      "motif_58_sample_1.wav",
+      "motif_58_sample_2.wav"
+    ]
+  },
+  {
+    "id": 71,
+    "symbol": "M71",
+    "count": 78888,
+    "samples": [
+      "motif_71_sample_0.wav",
+      "motif_71_sample_1.wav",
+      "motif_71_sample_2.wav"
+    ]
+  },
+  {
+    "id": 12,
+    "symbol": "M12",
+    "count": 77779,
+    "samples": [
+      "motif_12_sample_0.wav",
+      "motif_12_sample_1.wav",
+      "motif_12_sample_2.wav"
+    ]
+  },
+  {
+    "id": 47,
+    "symbol": "M47",
+    "count": 75995,
+    "samples": [
+      "motif_47_sample_0.wav",
+      "motif_47_sample_1.wav",
+      "motif_47_sample_2.wav"
+    ]
+  },
+  {
+    "id": 69,
+    "symbol": "M69",
+    "count": 75372,
+    "samples": [
+      "motif_69_sample_0.wav",
+      "motif_69_sample_1.wav",
+      "motif_69_sample_2.wav"
+    ]
+  },
+  {
+    "id": 87,
+    "symbol": "M87",
+    "count": 75172,
+    "samples": [
+      "motif_87_sample_0.wav",
+      "motif_87_sample_1.wav",
+      "motif_87_sample_2.wav"
+    ]
+  },
+  {
+    "id": 43,
+    "symbol": "M43",
+    "count": 75111,
+    "samples": [
+      "motif_43_sample_0.wav",
+      "motif_43_sample_1.wav",
+      "motif_43_sample_2.wav"
+    ]
+  },
+  {
+    "id": 45,
+    "symbol": "M45",
+    "count": 73551,
+    "samples": [
+      "motif_45_sample_0.wav",
+      "motif_45_sample_1.wav",
+      "motif_45_sample_2.wav"
+    ]
+  },
+  {
+    "id": 73,
+    "symbol": "M73",
+    "count": 72653,
+    "samples": [
+      "motif_73_sample_0.wav",
+      "motif_73_sample_1.wav",
+      "motif_73_sample_2.wav"
+    ]
+  },
+  {
+    "id": 38,
+    "symbol": "M38",
+    "count": 72115,
+    "samples": [
+      "motif_38_sample_0.wav",
+      "motif_38_sample_1.wav",
+      "motif_38_sample_2.wav"
+    ]
+  },
+  {
+    "id": 24,
+    "symbol": "M24",
+    "count": 72038,
+    "samples": [
+      "motif_24_sample_0.wav",
+      "motif_24_sample_1.wav",
+      "motif_24_sample_2.wav"
+    ]
+  },
+  {
+    "id": 97,
+    "symbol": "M97",
+    "count": 71721,
+    "samples": [
+      "motif_97_sample_0.wav",
+      "motif_97_sample_1.wav",
+      "motif_97_sample_2.wav"
+    ]
+  },
+  {
+    "id": 75,
+    "symbol": "M75",
+    "count": 71007,
+    "samples": [
+      "motif_75_sample_0.wav",
+      "motif_75_sample_1.wav",
+      "motif_75_sample_2.wav"
+    ]
+  },
+  {
+    "id": 64,
+    "symbol": "M64",
+    "count": 70742,
+    "samples": [
+      "motif_64_sample_0.wav",
+      "motif_64_sample_1.wav",
+      "motif_64_sample_2.wav"
+    ]
+  },
+  {
+    "id": 6,
+    "symbol": "M06",
+    "count": 68344,
+    "samples": [
+      "motif_06_sample_0.wav",
+      "motif_06_sample_1.wav",
+      "motif_06_sample_2.wav"
+    ]
+  },
+  {
+    "id": 35,
+    "symbol": "M35",
+    "count": 68329,
+    "samples": [
+      "motif_35_sample_0.wav",
+      "motif_35_sample_1.wav",
+      "motif_35_sample_2.wav"
+    ]
+  },
+  {
+    "id": 60,
+    "symbol": "M60",
+    "count": 66739,
+    "samples": [
+      "motif_60_sample_0.wav",
+      "motif_60_sample_1.wav",
+      "motif_60_sample_2.wav"
+    ]
+  },
+  {
+    "id": 63,
+    "symbol": "M63",
+    "count": 66027,
+    "samples": [
+      "motif_63_sample_0.wav",
+      "motif_63_sample_1.wav",
+      "motif_63_sample_2.wav"
+    ]
+  },
+  {
+    "id": 70,
+    "symbol": "M70",
+    "count": 65909,
+    "samples": [
+      "motif_70_sample_0.wav",
+      "motif_70_sample_1.wav",
+      "motif_70_sample_2.wav"
+    ]
+  },
+  {
+    "id": 17,
+    "symbol": "M17",
+    "count": 64871,
+    "samples": [
+      "motif_17_sample_0.wav",
+      "motif_17_sample_1.wav",
+      "motif_17_sample_2.wav"
+    ]
+  },
+  {
+    "id": 11,
+    "symbol": "M11",
+    "count": 64431,
+    "samples": [
+      "motif_11_sample_0.wav",
+      "motif_11_sample_1.wav",
+      "motif_11_sample_2.wav"
+    ]
+  },
+  {
+    "id": 98,
+    "symbol": "M98",
+    "count": 64399,
+    "samples": [
+      "motif_98_sample_0.wav",
+      "motif_98_sample_1.wav",
+      "motif_98_sample_2.wav"
+    ]
+  },
+  {
+    "id": 49,
+    "symbol": "M49",
+    "count": 63295,
+    "samples": [
+      "motif_49_sample_0.wav",
+      "motif_49_sample_1.wav",
+      "motif_49_sample_2.wav"
+    ]
+  },
+  {
+    "id": 89,
+    "symbol": "M89",
+    "count": 62611,
+    "samples": [
+      "motif_89_sample_0.wav",
+      "motif_89_sample_1.wav",
+      "motif_89_sample_2.wav"
+    ]
+  },
+  {
+    "id": 48,
+    "symbol": "M48",
+    "count": 60964,
+    "samples": [
+      "motif_48_sample_0.wav",
+      "motif_48_sample_1.wav",
+      "motif_48_sample_2.wav"
+    ]
+  },
+  {
+    "id": 79,
+    "symbol": "M79",
+    "count": 59400,
+    "samples": [
+      "motif_79_sample_0.wav",
+      "motif_79_sample_1.wav",
+      "motif_79_sample_2.wav"
+    ]
+  },
+  {
+    "id": 80,
+    "symbol": "M80",
+    "count": 59005,
+    "samples": [
+      "motif_80_sample_0.wav",
+      "motif_80_sample_1.wav",
+      "motif_80_sample_2.wav"
+    ]
+  },
+  {
+    "id": 68,
+    "symbol": "M68",
+    "count": 58914,
+    "samples": [
+      "motif_68_sample_0.wav",
+      "motif_68_sample_1.wav",
+      "motif_68_sample_2.wav"
+    ]
+  },
+  {
+    "id": 74,
+    "symbol": "M74",
+    "count": 58412,
+    "samples": [
+      "motif_74_sample_0.wav",
+      "motif_74_sample_1.wav",
+      "motif_74_sample_2.wav"
+    ]
+  },
+  {
+    "id": 59,
+    "symbol": "M59",
+    "count": 55449,
+    "samples": [
+      "motif_59_sample_0.wav",
+      "motif_59_sample_1.wav",
+      "motif_59_sample_2.wav"
+    ]
+  },
+  {
+    "id": 95,
+    "symbol": "M95",
+    "count": 55419,
+    "samples": [
+      "motif_95_sample_0.wav",
+      "motif_95_sample_1.wav",
+      "motif_95_sample_2.wav"
+    ]
+  },
+  {
+    "id": 50,
+    "symbol": "M50",
+    "count": 54410,
+    "samples": [
+      "motif_50_sample_0.wav",
+      "motif_50_sample_1.wav",
+      "motif_50_sample_2.wav"
+    ]
+  },
+  {
+    "id": 36,
+    "symbol": "M36",
+    "count": 53326,
+    "samples": [
+      "motif_36_sample_0.wav",
+      "motif_36_sample_1.wav",
+      "motif_36_sample_2.wav"
+    ]
+  },
+  {
+    "id": 25,
+    "symbol": "M25",
+    "count": 51538,
+    "samples": [
+      "motif_25_sample_0.wav",
+      "motif_25_sample_1.wav",
+      "motif_25_sample_2.wav"
+    ]
+  },
+  {
+    "id": 77,
+    "symbol": "M77",
+    "count": 49691,
+    "samples": [
+      "motif_77_sample_0.wav",
+      "motif_77_sample_1.wav",
+      "motif_77_sample_2.wav"
+    ]
+  },
+  {
+    "id": 0,
+    "symbol": "M00",
+    "count": 49316,
+    "samples": [
+      "motif_00_sample_0.wav",
+      "motif_00_sample_1.wav",
+      "motif_00_sample_2.wav"
+    ]
+  },
+  {
+    "id": 37,
+    "symbol": "M37",
+    "count": 45553,
+    "samples": [
+      "motif_37_sample_0.wav",
+      "motif_37_sample_1.wav",
+      "motif_37_sample_2.wav"
+    ]
+  },
+  {
+    "id": 61,
+    "symbol": "M61",
+    "count": 43112,
+    "samples": [
+      "motif_61_sample_0.wav",
+      "motif_61_sample_1.wav",
+      "motif_61_sample_2.wav"
+    ]
+  },
+  {
+    "id": 40,
+    "symbol": "M40",
+    "count": 38997,
+    "samples": [
+      "motif_40_sample_0.wav",
+      "motif_40_sample_1.wav",
+      "motif_40_sample_2.wav"
+    ]
+  },
+  {
+    "id": 91,
+    "symbol": "M91",
+    "count": 38963,
+    "samples": [
+      "motif_91_sample_0.wav",
+      "motif_91_sample_1.wav",
+      "motif_91_sample_2.wav"
+    ]
+  },
+  {
+    "id": 51,
+    "symbol": "M51",
+    "count": 38097,
+    "samples": [
+      "motif_51_sample_0.wav",
+      "motif_51_sample_1.wav",
+      "motif_51_sample_2.wav"
+    ]
+  },
+  {
+    "id": 88,
+    "symbol": "M88",
+    "count": 36879,
+    "samples": [
+      "motif_88_sample_0.wav",
+      "motif_88_sample_1.wav",
+      "motif_88_sample_2.wav"
+    ]
+  },
+  {
+    "id": 9,
+    "symbol": "M09",
+    "count": 36800,
+    "samples": [
+      "motif_09_sample_0.wav",
+      "motif_09_sample_1.wav",
+      "motif_09_sample_2.wav"
+    ]
+  },
+  {
+    "id": 94,
+    "symbol": "M94",
+    "count": 34085,
+    "samples": [
+      "motif_94_sample_0.wav",
+      "motif_94_sample_1.wav",
+      "motif_94_sample_2.wav"
+    ]
+  },
+  {
+    "id": 21,
+    "symbol": "M21",
+    "count": 33962,
+    "samples": [
+      "motif_21_sample_0.wav",
+      "motif_21_sample_1.wav",
+      "motif_21_sample_2.wav"
+    ]
+  },
+  {
+    "id": 54,
+    "symbol": "M54",
+    "count": 33031,
+    "samples": [
+      "motif_54_sample_0.wav",
+      "motif_54_sample_1.wav",
+      "motif_54_sample_2.wav"
+    ]
+  },
+  {
+    "id": 28,
+    "symbol": "M28",
+    "count": 29983,
+    "samples": [
+      "motif_28_sample_0.wav",
+      "motif_28_sample_1.wav",
+      "motif_28_sample_2.wav"
+    ]
+  },
+  {
+    "id": 13,
+    "symbol": "M13",
+    "count": 27697,
+    "samples": [
+      "motif_13_sample_0.wav",
+      "motif_13_sample_1.wav",
+      "motif_13_sample_2.wav"
+    ]
+  },
+  {
+    "id": 41,
+    "symbol": "M41",
+    "count": 22240,
+    "samples": [
+      "motif_41_sample_0.wav",
+      "motif_41_sample_1.wav",
+      "motif_41_sample_2.wav"
+    ]
+  },
+  {
+    "id": 96,
+    "symbol": "M96",
+    "count": 22086,
+    "samples": [
+      "motif_96_sample_0.wav",
+      "motif_96_sample_1.wav",
+      "motif_96_sample_2.wav"
+    ]
+  },
+  {
+    "id": 3,
+    "symbol": "M03",
+    "count": 20458,
+    "samples": [
+      "motif_03_sample_0.wav",
+      "motif_03_sample_1.wav",
+      "motif_03_sample_2.wav"
+    ]
+  },
+  {
+    "id": 33,
+    "symbol": "M33",
+    "count": 17330,
+    "samples": [
+      "motif_33_sample_0.wav",
+      "motif_33_sample_1.wav",
+      "motif_33_sample_2.wav"
+    ]
+  },
+  {
+    "id": 86,
+    "symbol": "M86",
+    "count": 15110,
+    "samples": [
+      "motif_86_sample_0.wav",
+      "motif_86_sample_1.wav",
+      "motif_86_sample_2.wav"
+    ]
+  },
+  {
+    "id": 83,
+    "symbol": "M83",
+    "count": 13555,
+    "samples": [
+      "motif_83_sample_0.wav",
+      "motif_83_sample_1.wav",
+      "motif_83_sample_2.wav"
+    ]
+  }
+];
